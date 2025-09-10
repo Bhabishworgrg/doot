90cd4129f17b8ee49d070da9a0c92210e63bbdd2
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-09-10 20:00:58 +0545

    Create a tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener: TcpListener = match TcpListener::bind("127.0.0.1:6969") {
+        Ok(listener) => listener,
+        Err(_) => {
+            println!("Failed to create a TCP listener");
+            return;
+        }
+    };
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_) => println!("Accepted a new connection"),
+            Err(_) => println!("Error accepting a connection")
+        }
+    }
 }

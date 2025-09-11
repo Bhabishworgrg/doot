31ad7ecc94fbc58ef26b08bb8891f3605d9c058d
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-09-11 16:31:54 +0545

    Define constants

---
+++ src/constants/config.rs
@@ -0,0 +1,3 @@
+pub const HOST: &str = "127.0.0.1";
+pub const PORT: u16 = 6969;
+pub const MAX_REQ_SIZE: usize = 8192;
---
+++ src/constants/status_code.rs
@@ -0,0 +1,2 @@
+pub const OK: &str = "200 OK";
+pub const NOT_FOUND: &str = "404 Not Found";
